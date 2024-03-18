13a667d0823da097ad73fbac375ecf353d46707f
ahk0419 <kinsin3@gmail.com>
2024-03-18 17:43:31 +0900

    Deposit account

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -7,9 +7,15 @@ type Account struct {
 }
 
 //누구나 나의 Balance를 0 마음대로 조정하지 못하도록 대문자를 소문자로 지정후 퍼블릭을 만든다
-
+//실제 메모리 account를 return
 // NewAccount create Account
 func NewAccount(owner string) *Account{
 	account := Account{owner: owner, balance: 0}
 	return &account
+}
+
+//balance ++ 증가 /-- 감소
+//Deposit x amount onyour account
+func (a Account) Deposit(amount int) {
+	a.balance += amount
 }
--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/ahk0419/go-project/accounts"
+)
 
 func main() {
 	account := accounts.NewAccount("ahk0419")
+	account.Deposit(10)
+
 	fmt.Println(account)
 }
